ea9ecfb89626fd8f14f89f826ef0d264c73d3ebe
exploding60 <rizkysyahputra14045@gmail.com>
2023-01-02 23:46:37 +0700

    Adding SOmething

--- src/controller/addskill.js
+++ src/controller/addskill.js
@@ -4,57 +4,48 @@ const createError = require("http-errors");
 const { v4: uuidv4 } = require("uuid");
 const Port = process.env.PORT;
 const Host = process.env.HOST;
-const {
-    insertSkill,
-    DeleteSkill,
-    GetSkill
-} = require("../model/addskill") 
+const { insertSkill, DeleteSkill, GetSkill } = require("../model/addskill");
 
 const addSkillController = {
-    Insert: async(req,res,next) => {
-        try {
-            const { skill } = req.body;
-            const { id } = req.payload;
-            const users_id = id
-            const data = {
-                users_id,
-                skill
-            }
-            await insertSkill(data)
-            response(res, 200, true, data, "input skill success");
-        } catch (error){
-            console.log(error);
-            response(res, 404, false, "input skill failed");
-        }
-    }, Delete: async(req,res,next) => {
-        try {
-            const { skill } = req.body;
-            const { id } = req.payload;
-            const users_id = id
-            const data = {
-                users_id,
-                skill
-            }
-            await DeleteSkill(data)
-            response(res, 200, true, data, "delete skill success");
-        } catch (error){
-            console.log(error);
-            response(res, 404, false, "delete skill failed");
-        }
-    }, Get: async(req,res,next) => {
-        try {
-            const { id } = req.payload;
-            const users_id = id
-            const data = {
-                users_id,
-            }
-            const result = await GetSkill(data)
-            response(res, 200, true, result.rows, "get skill success");
-        } catch (error){
-            console.log(error);
-            response(res, 404, false, "get skill failed");
-        }
+  Insert: async (req, res, next) => {
+    try {
+      const { skill } = req.body;
+      const { id } = req.payload;
+      const users_id = id;
+      const data = {
+        users_id,
+        skill,
+      };
+      await insertSkill(data);
+      response(res, 200, true, data, "input skill success");
+    } catch (error) {
+      console.log(error);
+      response(res, 404, false, "input skill failed");
     }
-}
+  },
+  Delete: async (req, res, next) => {
+    try {
+      await DeleteSkill(req.params.id);
+      response(res, 200, true, [], "delete skill success");
+    } catch (error) {
+      console.log(error);
+      response(res, 404, false, "delete skill failed");
+    }
+  },
+  Get: async (req, res, next) => {
+    try {
+      const { id } = req.payload;
+      const users_id = id;
+      const data = {
+        users_id,
+      };
+      const result = await GetSkill(data);
+      response(res, 200, true, result.rows, "get skill success");
+    } catch (error) {
+      console.log(error);
+      response(res, 404, false, "get skill failed");
+    }
+  },
+};
 
 exports.addSkillController = addSkillController;
--- src/routes/addskill.js
+++ src/routes/addskill.js
@@ -2,11 +2,14 @@ const express = require(`express`);
 const router = express.Router();
 const { protect } = require("../middleware/auth");
 const upload = require("../middleware/upload");
-const addSkillController = require("../controller/addskill")
+const addSkillController = require("../controller/addskill");
 
-router.post("/add",protect, addSkillController.addSkillController.Insert);
-router.delete("/delete",protect,addSkillController.addSkillController.Delete)
-router.get("/get",protect,addSkillController.addSkillController.Get)
-
+router.post("/add", protect, addSkillController.addSkillController.Insert);
+router.delete(
+  "/delete/:id",
+  protect,
+  addSkillController.addSkillController.Delete
+);
+router.get("/get", protect, addSkillController.addSkillController.Get);
 
 module.exports = router;
